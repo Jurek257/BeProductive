565c411f944471463c0c4cb2673ef476c344e980
Yurii <lobacovurij@gmail.com>
2025-12-03 18:56:33 +0100

    added sound Terminator

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,37 +1,77 @@
-import { StyleSheet , Pressable } from 'react-native';
+import { StyleSheet, Pressable } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import * as Progress from 'react-native-progress';
-import { useState } from 'react';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as Progress from "react-native-progress";
+import { useRef, useState } from "react";
+import { Audio } from "expo-av";
 
 export default function TabOneScreen() {
-  const [timerProgress, setTimerProgress] = useState(1);
-  const [isTimerRunning,setTimerRunning] = useState(false);
+  const totalTime: number = 30;
+  const [timerProgress, setTimerProgress] = useState(totalTime);
+  const [isTimerRunning, setTimerRunning] = useState(false);
+  const TimerID = useRef<number | null>(null);
 
-  const onTimerPress = () => {
-    setTimerProgress(prev => Math.max(prev - 0.05 , 0));
+  const getStringMinutesAndSeconds = (timerProgress: number) => {
+    const minutes = Math.floor(timerProgress / 60);
+    const secondsRemaining = timerProgress % 60;
+    return `${minutes}:${secondsRemaining.toString().padStart(2, "0")}`;
+  };
+
+  const PlaySound = async () => {
+    const { sound } = await Audio.Sound.createAsync(
+      require("../../assets/sounds/timer-terminer-342934.mp3"),
+    );
+    await sound.playAsync();
+  };
+
+  const setTimerState = () => {
+    if (isTimerRunning) {
+      return;
+    }
+
+    setTimerRunning(true);
+
+    TimerID.current = setInterval(() => {
+      setTimerProgress((previousTime) => {
+        const newTime = previousTime - 1;
+        if (newTime === 0 && TimerID.current) {
+          clearInterval(TimerID.current);
+          TimerID.current = null;
+          setTimerRunning(false);
+          PlaySound();
+        }
+        return newTime;
+      });
+    }, 1000);
   };
 
   return (
     <SafeAreaView style={localStyles.screen}>
+      <Pressable onPress={setTimerState}>
+        <Progress.Pie
+          progress={timerProgress / totalTime}
+          size={300}
+          indeterminate={false}
+          color="rgba(255,255,255,1)"
+        />
+      </Pressable>
 
-    <Pressable onPress={onTimerPress}>
-      <Progress.Pie progress={timerProgress} size={300} indeterminate={false} color="rgba(255,255,255,1)"/>
-    </Pressable>
-    
-      <Text style={localStyles.timeLeft}>25:00</Text>
-    </SafeAreaView>);
+      <Text style={localStyles.timeLeft}>
+        {getStringMinutesAndSeconds(timerProgress)}
+      </Text>
+    </SafeAreaView>
+  );
 }
 
 const localStyles = StyleSheet.create({
   screen: {
-    flex:1 ,
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   timeLeft: {
-    fontSize:30
-  }
- });
+    fontSize: 30,
+  },
+});
